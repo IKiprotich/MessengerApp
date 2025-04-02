2c9fc51ab5ae6a2484b6a0723739d000192a2830
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-02 10:42:35 +0300

    Initial Commit

---
+++ MessengerApp/MessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerAppApp.swift
+//  MessengerApp
+//
+//  Created by Ian   on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
