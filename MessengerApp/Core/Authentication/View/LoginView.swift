89098190214ddfb1d652bba4883e1c9dda029a5b
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-02 11:19:17 +0300

    Buttons and text fields

--- MessengerApp/Core/Authentication/View/LoginView.swift
+++ MessengerApp/Core/Authentication/View/LoginView.swift
@@ -8,15 +8,37 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
         NavigationStack{
             VStack{
                 //Logo Image
-                
+                Image("Messenger")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .padding()
                 
                 //Text fields
+                VStack
+                {
+                    TextField("Enter your email", text:$email)
+                        .padding()
+                    SecureField("Enter your password", text:$password)
+                        .padding()
+                }
                 
                 //Forgot password
+                Button {
+                    print("Forgot Password")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                }
                 
                 //Log in button
                 
