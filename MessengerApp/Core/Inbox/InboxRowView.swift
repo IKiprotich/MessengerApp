86c269b747d715e533c85d11f03ce22388eb7634
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-04 19:52:09 +0300

    done with the skeleton for top part

---
+++ MessengerApp/Core/Inbox/InboxRowView.swift
@@ -0,0 +1,20 @@
+//
+//  InboxRowView.swift
+//  MessengerApp
+//
+//  Created by Ian   on 04/04/2025.
+//
+
+import SwiftUI
+
+struct InboxRowView: View {
+    var body: some View {
+        HStack{
+            
+        }
+    }
+}
+
+#Preview {
+    InboxRowView()
+}
