e9fa7a5cc84c6fae11ea8eef9d2220cdc9dbe475
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-04 19:45:33 +0300

    sample alignment

---
+++ MessengerApp/Core/ActiveNow/ActiveNowView.swift
@@ -0,0 +1,40 @@
+//
+//  ActiveNowView.swift
+//  MessengerApp
+//
+//  Created by Ian   on 04/04/2025.
+//
+
+import SwiftUI
+
+struct ActiveNowView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false){
+            HStack(spacing: 32){
+                ForEach(0 ... 10, id: \.self){
+                    user in
+                    VStack{
+                        ZStack(alignment: .bottomTrailing){
+                            Image(systemName: "person.circle.fill")
+                                .resizable()
+                                .frame(width: 64, height: 64)
+                                .foregroundColor(Color(.systemGray4))
+                            
+                            ZStack{
+                                Circle()
+                                    .fill(Color(.systemGreen))
+                                    .frame(width: 112, height: 12)
+                                
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ActiveNowView()
+}
