550a484e5aed3e0999ea4d1d498e90bcc8fa0ff2
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-04 20:03:59 +0300

    row view done

--- MessengerApp/Core/Inbox/InboxRowView.swift
+++ MessengerApp/Core/Inbox/InboxRowView.swift
@@ -9,9 +9,37 @@ import SwiftUI
 
 struct InboxRowView: View {
     var body: some View {
-        HStack{
+        HStack(alignment: .top, spacing: 12){
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width:64 , height: 64)
+                .foregroundColor(Color(.systemGray4))
+            
+            VStack(alignment: .leading, spacing: 6){
+                Text("Ian Kiprotich")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                
+                
+                Text("Hello, how are you?")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .lineLimit(2)
+                    .frame(maxWidth: UIScreen.main.bounds.width - 100, alignment: .leading)
+            }
+            
+            HStack{
+                Text("Yesterday")
+                
+                Image(systemName: "chevron.right")
+            }
+            .font(.footnote)
+            .foregroundColor(.gray)
+            .fontWeight(.medium)
             
         }
+        .frame(height: 72)
+        .padding(.horizontal)
     }
 }
 
