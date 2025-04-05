12ba51ae34c49ddf3eebfedfa82bbd9794398775
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-05 12:34:15 +0300

    button and navigation bar

---
+++ MessengerApp/Core/NewMessage/NewMessageView.swift
@@ -0,0 +1,36 @@
+//
+//  NewMessageView.swift
+//  MessengerApp
+//
+//  Created by Ian   on 05/04/2025.
+//
+
+import SwiftUI
+
+struct NewMessageView: View {
+    @State private var searchText: String = ""
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        ScrollView{
+            TextField("To: ", text: $searchText)
+                .frame(height: 44)
+                .padding(.leading)
+                .background(Color(.systemGroupedBackground))
+        }
+        .navigationTitle("New Message")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            
+            ToolbarItem (placement: .navigationBarLeading) {
+                Button("Cancel") {
+                    dismiss()
+                }
+                .foregroundColor(.black)
+            }
+        }
+    }
+}
+
+#Preview {
+    NewMessageView()
+}
