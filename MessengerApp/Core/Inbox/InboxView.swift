4c3f9eeba5006a710a80ea6a242e4799efd5961b
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-05 12:23:44 +0300

    Starting compose message UI

--- MessengerApp/Core/Inbox/InboxView.swift
+++ MessengerApp/Core/Inbox/InboxView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct InboxView: View {
+    @State private var showNewMessageView = false
     var body: some View {
         NavigationStack{
             ScrollView{
@@ -22,6 +23,8 @@ struct InboxView: View {
                 .listStyle(PlainListStyle())
                 .frame(height:UIScreen.main.bounds.height - 120)
             }
+            .fullScreenCover(isPresented: $showNewMessageView, content: {Text("New Message")})
+            
             .toolbar{
                 ToolbarItem( placement: .navigationBarLeading){
                     HStack{
@@ -35,7 +38,7 @@ struct InboxView: View {
                 
                 ToolbarItem(placement: .navigationBarTrailing){
                     Button {
-                        print("Show new message view")
+                        showNewMessageView.toggle()
                     }  label: {
                     }
                         Image(systemName: "square.and.pencil.circle.fill")
