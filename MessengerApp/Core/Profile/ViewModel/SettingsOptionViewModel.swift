7ef9d3dee290028aceb02d21915a4ebda4339777
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-06 10:26:52 +0300

    added a swift file for options

---
+++ MessengerApp/Core/Profile/ViewModel/SettingsOptionViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsOptionViewModel.swift
+//  MessengerApp
+//
+//  Created by Ian   on 06/04/2025.
+//
+
+import Foundation
+enum SettingsOptionViewModel: Int, CaseIterable, Identifiable {
+    case darkmode
+    case activeStatus
+    case 
+    
+}
