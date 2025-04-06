d8c4ac7595430b6daa04499fd8b550355bf93754
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-06 10:27:08 +0300

    done with the skeleton

---
+++ MessengerApp/Core/Profile/View/ProfileView.swift
@@ -0,0 +1,65 @@
+//
+//  ProfileView.swift
+//  MessengerApp
+//
+//  Created by Ian   on 06/04/2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        
+        VStack{
+            //header
+            VStack{
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .foregroundColor(Color(.systemGray4))
+                
+                
+                Text("Kanye West")
+                    .font(.title)
+                    .fontWeight(.semibold)
+            }
+            
+            //list
+            
+            List{
+                Section{
+                    ForEach(0...5, id: \.self){option in
+                        HStack{
+                            Image(systemName: "bell.circle.fill")
+                                .resizable()
+                                .frame(width:24, height: 24)
+                                .foregroundColor(Color(.systemPurple))
+                            Text("Notifications")
+                                .font(.subheadline)
+                        }
+                    }
+                }
+                
+                Section{
+                    Button("Log Out") {
+                        
+                    }
+                    Button("Delete Account") {
+                        
+                    }
+                }
+                .foregroundColor(.red)
+                
+            }
+            
+            
+        }
+        
+        
+    }
+    
+}
+
+#Preview {
+    ProfileView()
+}
