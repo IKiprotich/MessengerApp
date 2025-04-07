8b701ffc935f005aa7e06f2441d2b19bfeda36d6
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-07 11:40:57 +0300

    a view model to add a picture to profile

---
+++ MessengerApp/Core/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileViewModel.swift
+//  MessengerApp
+//
+//  Created by Ian   on 07/04/2025.
+//
+
+import Foundation
+import SwiftUI
+import PhotosUI
+
+class ProfileViewModel: ObservableObject {
+    @Published var selectedItem: PhotosPickerItem?{
+        didSet{
+            Task{try await LoadImage()}
+        }
+    }
+    
+    @Published var profileImage: Image?
+    
+    func LoadImage() async throws {
+        guard let item = selectedItem else { return }
+        guard let imageData = try await item.loadTransferable(type: Data.self) else{return}
+        guard let uiImage = UIImage(data: imageData) else { return }
+        self.profileImage = Image(uiImage: uiImage)
+    }
+}
