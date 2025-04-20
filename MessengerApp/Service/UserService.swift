387ffb04620cc87b426a9e52a212f6f9aab1c2df
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-20 10:47:59 +0300

    fetch user data

--- MessengerApp/Service/UserService.swift
+++ MessengerApp/Service/UserService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestore
 
 class UserService{
     
@@ -16,5 +17,9 @@ class UserService{
     
     func fetchCurrentUser() async throws{
         guard let uid = Auth.auth().currentUser?.uid else {return}
+        let snapshot = try await Firestore.firestore().collection("users").document(uid).getDocument()
+        let user = try snapshot.data(as: User.self)
+        self.currentUser = user
+        print("DEBUG: Current user in service is \(currentUser)")
     }
 }
