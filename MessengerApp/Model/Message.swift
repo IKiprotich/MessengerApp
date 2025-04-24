a5f94849b60b3845b1e0b0f5726a98457af95371
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-24 10:39:07 +0300

    message data object

---
+++ MessengerApp/Model/Message.swift
@@ -0,0 +1,33 @@
+//
+//  Message.swift
+//  MessengerApp
+//
+//  Created by Ian   on 24/04/2025.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+import FirebaseAuth
+
+struct Message: Identifiable, Hashable, Codable {
+    @DocumentID var messageId: String?
+    let fromId: String
+    let toId: String
+    let messageText: String
+    let timeStamp: Timestamp
+    
+    var user: User?
+    
+    var id: String{
+        return messageId ?? NSUUID().uuidString
+    }
+    
+    var chatPartnerId: String{
+        return fromId == Auth.auth().currentUser?.uid ? toId : fromId
+    }
+    
+    var isFromCurrentUser: Bool{
+        return fromId == Auth.auth().currentUser?.uid
+    }
+}
