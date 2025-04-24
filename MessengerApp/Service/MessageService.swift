527e306ff92bb1fce1331f350976cfa3ad6f4dd9
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-24 11:01:04 +0300

    message service

---
+++ MessengerApp/Service/MessageService.swift
@@ -0,0 +1,40 @@
+//
+//  MessageService.swift
+//  MessengerApp
+//
+//  Created by Ian   on 24/04/2025.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
+
+struct MessageService {
+    
+    let messagesCollection = Firestore.firestore().collection("messages")
+    
+    func sendMessage(_ messageText: String, toUser user: User) {
+        guard let currentUid = Auth.auth().currentUser?.uid else { return }
+        let chatPartnerId = user.id
+        
+        let currentUserRef = messagesCollection.document(currentUid).collection(chatPartnerId).document()
+        let chatPartnerRef = messagesCollection.document(chatPartnerId).collection(currentUid).document()
+        
+        let messageId = currentUserRef.documentID
+        
+        let message = Message(messageId: messageId,
+                              fromId: currentUid,
+                              toId: chatPartnerId,
+                              messageText: messageText,
+                              timeStamp: Timestamp()
+        )
+        
+        guard let messageData = try? Firestore.Encoder().encode(message) else { return }
+        
+        currentUserRef.setData(messageData)
+        chatPartnerRef.document(messageId).setData(messageData)
+        
+        
+    }
+}
